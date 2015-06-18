c204482e3ff5e216ed37d3f8359b3d88a6887553
Lucas van Dijk <info@return1.net>
2015-06-18 21:32:00 +0200

    Modify D2 statistics so that each function has the same calling interface

--- afphylogeny/statistics/d2.py
+++ afphylogeny/statistics/d2.py
@@ -44,48 +44,61 @@ def d2(seq1_iter, seq2_iter):
     return sum([word_count1[word] * word_count2[word] for word in word_count1])
 
 
-def d2_normalised(seq1_iter, seq2_iter, probs1, probs2):
-    word_count1 = defaultdict(int)
-    word_count2 = defaultdict(int)
+class D2Normalised:
+    def __init__(self, probs1, probs2):
+        self.probs1 = probs1
+        self.probs2 = probs2
 
-    for word in seq1_iter:
-        word_count1[word] += 1
+    def __call__(self, seq1_iter, seq2_iter):
+        word_count1 = defaultdict(int)
+        word_count2 = defaultdict(int)
 
-    for word in seq2_iter:
-        word_count2[word] += 1
+        for word in seq1_iter:
+            word_count1[word] += 1
 
-    n = len(word_count1)
-    m = len(word_count2)
+        for word in seq2_iter:
+            word_count2[word] += 1
 
-    score = 0.0
+        n = len(word_count1)
+        m = len(word_count2)
 
-    for word in word_count1:
-        normalised1 = word_count1[word] - (n * probs1[word])
-        normalised2 = word_count2[word] - (m * probs2[word])
+        score = 0.0
 
-        score += (normalised1 * normalised2) / math.sqrt(
-            normalised1**2 + normalised2**2
-        )
+        for word in word_count1:
+            normalised1 = word_count1[word] - (n * self.probs1[word])
+            normalised2 = word_count2[word] - (m * self.probs2[word])
 
-    return score
+            score += (normalised1 * normalised2) / math.sqrt(
+                normalised1**2 + normalised2**2
+            )
 
-
-def _get_neighbourhood(word, alphabet):
-    for i in range(len(word)):
-        for letter in alphabet:
-            yield "".join(word[:i], letter, word[i+1:])
+        return score
 
 
-def d2_neighbourhood(seq1_iter, seq2_iter, alphabet="ACTG"):
-    word_count1 = defaultdict(int)
-    word_count2 = defaultdict(int)
+class D2Neighbourhood:
+    def __init__(self, alphabet="ACTG"):
+        self.alphabet = alphabet
 
-    for word in seq1_iter:
-        for neighbour in _get_neighbourhood(word, alphabet):
-            word_count1[neighbour] += 1
+    def _get_neighbourhood(word, alphabet):
+        for i in range(len(word)):
+            for letter in alphabet:
+                yield "".join(word[:i], letter, word[i+1:])
 
-    for word in seq2_iter:
-        for neighbour in _get_neighbourhood(word, alphabet):
-            word_count2[neighbour] += 1
+    def __call__(self, seq1_iter, seq2_iter):
+        word_count1 = defaultdict(int)
+        word_count2 = defaultdict(int)
 
-    return sum([word_count1[word] * word_count2[word] for word in word_count1])
+        for word in seq1_iter:
+            for neighbour in self._get_neighbourhood(word, self.alphabet):
+                word_count1[neighbour] += 1
+
+        for word in seq2_iter:
+            for neighbour in self._get_neighbourhood(word, self.alphabet):
+                word_count2[neighbour] += 1
+
+        return sum([
+            word_count1[word] * word_count2[word] for word in word_count1
+        ])
+
+d2_neighbourhood_dna = D2Neighbourhood()
+d2_neighbourhood_rna = D2Neighbourhood("ACUG")
